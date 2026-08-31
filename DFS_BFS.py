277b9d6d41fbc0051561d25fd2e92b4fd07a9f74
vatsalyamarsakatla-boop <vatsalyamarsakatla@gmail.com>
2026-08-31 22:23:54 +0530

    Add files via upload

---
+++ DFS_BFS.py
@@ -0,0 +1,45 @@
+def DFS(graph, vertex, visited):
+    visited[vertex] = True
+    print(vertex, end=" ")
+
+    for i in range(len(graph)):
+        if graph[vertex][i] == 1 and visited[i] == False:
+            DFS(graph, i, visited)
+
+
+def BFS(graph, start):
+    visited = [False] * len(graph)
+    queue = [start]
+    visited[start] = True
+
+    while queue:
+        vertex = queue.pop(0)
+        print(vertex, end=" ")
+
+        for i in range(len(graph)):
+            if graph[vertex][i] == 1 and visited[i] == False:
+                visited[i] = True
+                queue.append(i)
+
+
+n = int(input("Enter the number of vertices: "))
+
+print("Enter the adjacency matrix:")
+
+graph = []
+
+for i in range(n):
+    row = list(map(int, input().split()))
+    graph.append(row)
+
+start = int(input("Enter the starting vertex: "))
+
+visited = [False] * n
+
+print("\nDFS Traversal:")
+DFS(graph, start, visited)
+
+print("\nBFS Traversal:")
+BFS(graph, start)
+
+print("\nEN.No:9246018673")
